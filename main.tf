d7f0c6d3c1b80b19ba88ef2efc7186bb4dfbeca3
tiusoro <tiusoro@gmail.com>
2025-08-25 21:38:50 +0100

    update vpc

--- main.tf
+++ main.tf
@@ -14,13 +14,29 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "blog-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["af-south-1a", "af-south-1b", "af-south-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
+
+
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  instance_type = "t2.micro"
   key_name      = "user1"
 
 vpc_security_group_ids = [module.blog_sg.security_group_id]
@@ -30,12 +46,70 @@ vpc_security_group_ids = [module.blog_sg.security_group_id]
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = "vpc-abcde012"
+  subnets = ["subnet-abcde012", "subnet-bcde012a"]
+
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
+
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = "i-0f6d38a07d50d080f"
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+}
+
+
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
 
   name = "blog"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp", "ssh-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
