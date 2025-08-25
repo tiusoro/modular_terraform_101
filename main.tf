68276e77589f79d2209192c35c6ea285859cbc13
Anthony Usoro <33380362+tiusoro@users.noreply.github.com>
2025-08-25 22:03:42 +0100

    Update private subnets

--- main.tf
+++ main.tf
@@ -35,6 +35,7 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["af-south-1a", "af-south-1b", "af-south-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -130,4 +131,5 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 
 }
+
 
